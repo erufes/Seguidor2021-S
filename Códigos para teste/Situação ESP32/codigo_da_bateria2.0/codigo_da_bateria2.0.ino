b869e0a1aa20e46ddecfcf0b76f125cce95e93f4
Higor David O <43549329+hdogithub@users.noreply.github.com>
2021-01-19 21:13:20 -0300

    Atualização do código da bateria e resolução de conflito
    Feito os ultimos acertos e preparação para ser testada diretamente. Juntado as correções do código do Mayke ao meu código e resolvido o conflito.
    
    Co-Authored-By: Mayke Wallace <53350761+Nitrox0Af@users.noreply.github.com>

--- Códigos para teste/Situação ESP32/codigo_da_bateria2.0/codigo_da_bateria2.0.ino
+++ Códigos para teste/Situação ESP32/codigo_da_bateria2.0/codigo_da_bateria2.0.ino
@@ -1,25 +1,29 @@
 ////////////// Variaveis da Bateria //////////////
 #define AMOSTRAS 12                                                       // Quantidades de vezes que vai fazer a leitura da entrada 
-#define LED                                                               // LED designado a bateria
-#define EntradaTensao ?                                                   // Entrada de tensao(Porta Analogica)
+#define LED 4                                                              // LED designado a bateria
+#define EntradaTensao A0                                                   // Entrada de tensao(Porta Analogica)
 float V;                                                                  // Tensao adquirida
 float aRef = 3.3;                                                           // Tensão máxima que pode ser lida numa porta analógica (3 ou 5)
 float relacao = (143 / 43);
 
-////////////// setup() da Bateria //////////////
+////////////// Main e Funçoes auxiliares da Bateria //////////////
 
 float lePorta(uint8_t portaAnalogica);
 int verificaBateria();
 
+////////////// setup() da Bateria //////////////
+
 void setup() {
   pinMode(EntradaTensao, INPUT);
+  Serial.begin(115200);
 }
 
-////////////// Main e Funçoes auxiliares da Bateria //////////////
-
-void loop(){
-
-  
+void loop() {
+  Serial.print(" Verifica Bateria: ");
+  Serial.print(verificaBateria());
+  Serial.print(" e V: ");
+  Serial.println(V);
+  delay(500);  
 }
 
 float lePorta(uint8_t portaAnalogica) {
@@ -31,12 +35,11 @@ float lePorta(uint8_t portaAnalogica) {
   return total / (float)AMOSTRAS;
 }
 
-
 int verificaBateria() {
-  V = (lePorta(EntradaTensao) * aRef) / 1023.0);
+  V = (lePorta(EntradaTensao) * aRef) / 1023.0;
   V *= relacao;
   if (V <= 8.2 || V >= 8.4) {
-  return -1;
-}
-else return V;
+    return -1;
+  }
+  else return V;
 }
